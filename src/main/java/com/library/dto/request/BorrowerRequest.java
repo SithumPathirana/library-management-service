79a06545a72c32c82751c59bc9f51f1d78a2b468
Sithum <sithumk@getapplova.com>
2026-01-09 17:41:32 +0530

    Added dtos for book and borrower

---
+++ src/main/java/com/library/dto/request/BorrowerRequest.java
@@ -0,0 +1,23 @@
+package com.library.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BorrowerRequest {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+}
+
---
+++ src/main/java/com/library/dto/response/BorrowerResponse.java
@@ -0,0 +1,19 @@
+package com.library.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BorrowerResponse {
+    private UUID id;
+    private String name;
+    private String email;
+}
+
