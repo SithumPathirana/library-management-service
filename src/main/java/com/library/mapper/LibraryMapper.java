b78c3b9a4d8d48a67c9260fc9c6aa05fd9f0591d
Sithum <sithumk@getapplova.com>
2026-01-09 17:43:59 +0530

    Added mapper to convert dtos and models

---
+++ src/main/java/com/library/mapper/LibraryMapper.java
@@ -0,0 +1,60 @@
+package com.library.mapper;
+
+import com.library.dto.request.BookRequest;
+import com.library.dto.request.BorrowerRequest;
+import com.library.dto.response.BookResponse;
+import com.library.dto.response.BorrowerResponse;
+import com.library.model.Book;
+import com.library.model.Borrower;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LibraryMapper {
+
+    public Book toBook(BookRequest request) {
+        if (request == null) {
+            return null;
+        }
+        return Book.builder()
+                .isbn(request.getIsbn())
+                .title(request.getTitle())
+                .author(request.getAuthor())
+                .isBorrowed(false) // Default to not borrowed
+                .build();
+    }
+
+    public BookResponse toBookResponse(Book book) {
+        if (book == null) {
+            return null;
+        }
+        return BookResponse.builder()
+                .id(book.getId())
+                .isbn(book.getIsbn())
+                .title(book.getTitle())
+                .author(book.getAuthor())
+                .isBorrowed(book.isBorrowed())
+                .build();
+    }
+
+    public Borrower toBorrower(BorrowerRequest request) {
+        if (request == null) {
+            return null;
+        }
+        return Borrower.builder()
+                .name(request.getName())
+                .email(request.getEmail())
+                .build();
+    }
+
+    public BorrowerResponse toBorrowerResponse(Borrower borrower) {
+        if (borrower == null) {
+            return null;
+        }
+        return BorrowerResponse.builder()
+                .id(borrower.getId())
+                .name(borrower.getName())
+                .email(borrower.getEmail())
+                .build();
+    }
+}
+
